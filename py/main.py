ec0882151f6ec4124601be9b474f8bffe3c33726
ARASHI, Jumpei <jumpei.arashi@arashike.com>
2020-04-02 15:08:35 +0900

    add the usecase to find by user id

--- py/main.py
+++ py/main.py
@@ -1,22 +1,30 @@
 from fastapi import FastAPI
-from adapter.controller.http.user.register import UserRegisterController
-from adapter.controller.http.user.register import User
 from usecase.user.register import UserRegisterUsecase
+from adapter.controller.http.user.register import UserRegisterController
+from adapter.controller.http.user.register import Request
+
+from usecase.user.find import UserFindUsecase
+from adapter.controller.http.user.find import UserFindController
+
 from adapter.repository.uuid_id_probider import Uuid
 from adapter.repository.in_memory_user_repository import InMemory
-from helper import registerController
 
 app = FastAPI()
 
 findUsecase = UserFindUsecase(
-    InMemory()
+    InMemory(),
 )
-findController = UserFindController(usecase)
+findController = UserFindController(findUsecase)
+registerUsecase = UserRegisterUsecase(
+    Uuid(),
+    InMemory(),
+)
+registerController = UserRegisterController(registerUsecase)
 
 @app.get("/users/{user_id}")
 async def find_user(user_id: str):
     findController.handle(user_id)
 
 @app.post("/users/")
-async def register_user(user: User):
+async def register_user(user: Request):
     registerController.handle(user)
